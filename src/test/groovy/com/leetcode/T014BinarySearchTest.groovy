a30d1157c6f5f6391226e67a00ce948389aa0d2a
shubo zhang <shubozhang@shubos-air.lan>
2022-03-29 19:54:33 -0400

    added binary search

---
+++ src/test/groovy/com/leetcode/T014BinarySearchTest.groovy
@@ -0,0 +1,18 @@
+package com.leetcode
+
+import spock.lang.Specification
+
+class T014BinarySearchTest extends Specification {
+
+    def "test binary search on int array"() {
+        expect:
+        T014BinarySearch.search(arr as int[], target) == index
+
+        where:
+        arr                    | target || index
+        [1, 2, 3]              | 2      || 1
+        [1, 2, 3, 3, 4, 5, 10] | 10     || 6
+        [1, 2, 3, 3, 4, 5, 10] | 20     || -1
+        []                     | 20     || -1
+    }
+}
