9ab167f79af2a4017c4ca8bbdc861876941ed941
shubo zhang <shubozhang@shubos-air.lan>
2022-04-01 17:58:36 -0400

    added more binary search questions

---
+++ src/test/groovy/com/leetcode/L240Search2DMatrixIITest.groovy
@@ -0,0 +1,17 @@
+package com.leetcode
+
+import spock.lang.Specification
+
+class L240Search2DMatrixIITest extends Specification {
+
+    def "search target in 2D Matrix II"() {
+        expect:
+        L240Search2DMarixII.search(source as int[][], target) == result
+
+        where:
+        source                                                                                                | target || result
+        [[1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]] | 5      || true
+        [[1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]] | 20     || false
+
+    }
+}
---
+++ src/test/groovy/com/leetcode/T075FindPeakElementTest.groovy
@@ -0,0 +1,16 @@
+package com.leetcode
+
+import spock.lang.Specification
+
+class T075FindPeakElementTest extends Specification {
+
+    def "find peak element"() {
+        expect:
+        (result as int[]).contains(T075FindPeakElement.findPeak(arr as int[]))
+
+        where:
+        arr                      || result
+        [1, 2, 1, 3, 4, 5, 7, 6] || [1, 6]
+        [1, 2, 3, 4, 1]          || [3]
+    }
+}
