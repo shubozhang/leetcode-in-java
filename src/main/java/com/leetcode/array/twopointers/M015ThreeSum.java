09772bf2d9defc243d665bea2b4d4a42c8c3f8c9
shubo zhang <shubozhang@outlook.com>
2022-04-20 00:44:40 -0400

    added more content

--- src/main/java/com/leetcode/array/twopointers/M015ThreeSum.java
+++ src/main/java/com/leetcode/array/twopointers/M015ThreeSum.java
@@ -1,8 +1,8 @@
 package com.leetcode.array.twopointers;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import com.leetcode.array.T057ThreeSum;
+
+import java.util.*;
 
 /**
  https://leetcode.com/problems/3sum/
@@ -79,5 +79,68 @@ public class M015ThreeSum {
             }
         }
         return lists;
+    }
+
+    List<List<Integer>> results = new ArrayList<>();
+
+
+    private List<List<Integer>> threeSum(int[] A) {
+        if (A == null || A.length < 3) {
+            return results;
+        }
+
+        Arrays.sort(A);
+        // enumerate c, the maximum element
+        int i;
+        int n = A.length;
+        for (i = 2; i < n; ++i) {
+            // c is always the last in a bunch of duplicates
+            if (i + 1 < n && A[i + 1] == A[i]) {
+                continue;
+            }
+
+            // want to find all pairs of A[j]+A[k]=-A[i], such that
+            // j < k < i
+            twoSum(A, i - 1, -A[i]);
+        }
+        Collections.sort(results, new ListComparator<>());
+        return results;
+
+    }
+
+    private void twoSum(int[] A, int right, int target) {
+        int i, j;
+        j = right;
+        for (i = 0; i <= right; ++i) {
+            // A[i] must be the first in its duplicates
+            if (i > 0 && A[i] == A[i - 1]) { continue; }
+
+            while (j > i && A[j] > target - A[i]) { --j; }
+
+            if (i >= j) { break; }
+
+            if (A[i] + A[j] == target) {
+                List<Integer> t = new ArrayList<>();
+                t.add(A[i]);
+                t.add(A[j]);
+                t.add(-target); // t.add(A[right+1])
+                results.add(t);
+            }
+        }
+    }
+
+
+    // This is soring results using 1st number as 1st key, 2nd number as 2nd key...
+    class ListComparator<T extends Comparable<T>> implements Comparator<List<Integer>> {
+        @Override
+        public int compare(List<Integer> a, List<Integer> b) {
+            for (int i = 0; i < Math.min(a.size(), b.size()); i++) {
+                int c = a.get(i).compareTo(b.get(i));
+                if (c != 0) {
+                    return c;
+                }
+            }
+            return Integer.compare(a.size(), b.size());
+        }
     }
 }
