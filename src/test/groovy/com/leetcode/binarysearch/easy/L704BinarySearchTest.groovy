747705d8893a486f74cf4db02ec7d909ee161603
shubo zhang <shubozhang@outlook.com>
2022-04-20 08:15:44 -0400

    added more content

--- src/test/groovy/com/leetcode/binarysearch/easy/L704BinarySearchTest.groovy
+++ src/test/groovy/com/leetcode/binarysearch/easy/L704BinarySearchTest.groovy
@@ -4,15 +4,16 @@ import spock.lang.Specification
 
 class L704BinarySearchTest extends Specification {
 
-    def "test binary search on int array"() {
+    def "test binary search in int array"() {
         expect:
         L704BinarySearch.search(arr as int[], target) == index
 
         where:
-        arr                    | target || index
-        [1, 2, 3]              | 2      || 1
-        [1, 2, 3, 3, 4, 5, 10] | 10     || 6
-        [1, 2, 3, 3, 4, 5, 10] | 20     || -1
-        []                     | 20     || -1
+        arr                                              | target || index
+        [1, 2, 3]                                        | 2      || 1
+        [1, 2, 3, 3, 4, 5, 10]                           | 10     || 6
+        [1, 2, 3, 3, 4, 5, 10]                           | 20     || -1
+        []                                               | 20     || -1
+        [1, 2, 3, 4, 5, 6, 7, 8, 10, 100, 150, 156, 179] | 9      || -1
     }
 }
