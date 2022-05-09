06082b12cf93371ddb6a10ca6e5bf78ee84b1fd4
shubo zhang <shubozhang@outlook.com>
2022-05-08 21:45:35 -0400

    added more content

--- src/main/java/com/leetcode/linkedlist/M142LinkedListCycleII.java
+++ src/main/java/com/leetcode/linkedlist/M142LinkedListCycleII.java
@@ -1,4 +1,33 @@
 package com.leetcode.linkedlist;
 
+import com.model.ListNode;
+
 public class M142LinkedListCycleII {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                fast = head;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+
+                    if (slow == fast) {
+                        return fast;
+                    }
+                }
+                return fast;
+            }
+        }
+        return null;
+    }
 }
